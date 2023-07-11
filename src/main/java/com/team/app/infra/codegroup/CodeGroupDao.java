c03fdb284b3ca43a3c2f9e06099a54868df4abeb
ArtVandelay500 <ArtVandelay500@users.noreply.github.com>
2023-07-11 17:39:31 +0900

    basic folder structure

---
+++ src/main/java/com/team/app/infra/codegroup/CodeGroupDao.java
@@ -0,0 +1,5 @@
+package com.team.app.infra.codegroup;
+
+public class CodeGroupDao {
+
+}
---
+++ src/main/java/com/team/app/infra/codegroup/CodeGroupServiceImpl.java
@@ -0,0 +1,5 @@
+package com.team.app.infra.codegroup;
+
+public class CodeGroupServiceImpl implements CodeGroupService {
+
+}
