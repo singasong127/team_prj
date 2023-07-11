f91c98633c2ceaeaebf2ae431488e452fca82e70
ArtVandelay500 <ArtVandelay500@users.noreply.github.com>
2023-07-11 17:40:17 +0900

    이하동

--- src/main/java/com/team/app/infra/codegroup/CodeGroupController.java
+++ src/main/java/com/team/app/infra/codegroup/CodeGroupController.java
@@ -1,5 +1,8 @@
 package com.team.app.infra.codegroup;
 
+import org.springframework.stereotype.Controller;
+
+@Controller
 public class CodeGroupController {
 
 }
--- src/main/java/com/team/app/infra/codegroup/CodeGroupDao.java
+++ src/main/java/com/team/app/infra/codegroup/CodeGroupDao.java
@@ -1,5 +1,8 @@
 package com.team.app.infra.codegroup;
 
+import org.springframework.stereotype.Repository;
+
+@Repository
 public class CodeGroupDao {
 
 }
