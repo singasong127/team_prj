735a132a13b82b118fcda7eee8dd5c57085937f2
ArtVandelay500 <ArtVandelay500@users.noreply.github.com>
2023-07-12 17:03:58 +0900

    Controller to index warp

--- src/main/java/com/team/app/infra/codegroup/CodeGroupController.java
+++ src/main/java/com/team/app/infra/codegroup/CodeGroupController.java
@@ -1,10 +1,13 @@
 package com.team.app.infra.codegroup;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class CodeGroupController {
 
-	
-
+@RequestMapping("/")
+    public String index(){
+    return "index";
+}
 }
