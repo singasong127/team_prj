ef40743db7a90b8940b06ea7bc796f96d716f8d1
singasong127 <songjy0127@naver.com>
2023-07-26 16:30:20 +0900

    코드 그룹 수정 사항 커밋

--- src/main/java/com/team/app/infra/codegroup/CodeGroupController.java
+++ src/main/java/com/team/app/infra/codegroup/CodeGroupController.java
@@ -1,13 +1,94 @@
 package com.team.app.infra.codegroup;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+
 
 @Controller
 public class CodeGroupController {
 
-@RequestMapping("/")
-    public String index(){
-    return "usr/infra/index/index";
-}
+	@Autowired
+	CodeGroupServiceImpl service;
+	
+	@RequestMapping(value="/cglist")
+	public String codeGroupList(CodeGroupVo vo, Model model) {
+		
+		List<CodeGroup> list = service.selectList(vo);
+		
+//		왼쪽의 list는 jsp에서 사용할 변수명
+		model.addAttribute("list", list);
+		
+		return "admin/infra/codeGroup/codeGroupList";
+	}
+	
+	/*
+	 * @RequestMapping(value="/cglist") public String
+	 * codeGroupList(@ModelAttribute("vo") CodeGroupVo vo, Model model) {
+	 * 
+	 * vo.setShKey(vo.getShKey() == null ? "" : vo.getShKey());
+	 * 
+	 * vo.setParamsPaging(service.selectOneCount(vo));
+	 * 
+	 * if(vo.getTotalRows() > 0) { List<CodeGroup> list = service.selectList(vo);
+	 * model.addAttribute("list", list); // model.addAttribute("vo", vo); } else {
+	 * // by pass }
+	 * 
+	 * return "xdm/infra/codegroup/codeGroupXdmList"; }
+	 */
+	
+
+	
+	/*
+	 * @RequestMapping(value="/cgform") public String codeGroupForm(CodeGroupVo vo,
+	 * Model model) { CodeGroup codeGroup = service.selectOne(vo);
+	 * 
+	 * model.addAttribute("item", codeGroup);
+	 * 
+	 * return "xdm/infra/codegroup/codeGroupForm"; }
+	 */
+	
+	@RequestMapping(value="/cgupdate")
+	public String codeGroupUpdate(CodeGroup dto) {
+		
+		System.out.println("codeGroupUpdate");
+		
+		service.update(dto);
+		
+		return "redirect:/cglist";
+	}
+	
+	@RequestMapping(value="/cguelect")
+	public String codeGroupUelect(CodeGroup dto) {
+		
+		System.out.println("Uelect");
+		
+		service.uelect(dto);
+		
+		return "redirect:/cglist";
+	}
+	
+	@RequestMapping(value="/cgdelete")
+	public String codeGroupDelete(CodeGroup dto) {
+		
+		System.out.println("Deleted");
+		
+		service.delete(dto);
+		
+		return "redirect:/cglist";
+	}
+	
+	@RequestMapping(value="/cginsert")
+	public String codeGroupInsert(CodeGroup dto) {
+		
+		System.out.println("Inserted");
+		
+		service.insert(dto);
+		
+		return "redirect:/cglist";
+	}
 }
--- src/main/webapp/WEB-INF/views/admin/infra/codeGroup/codeGroupList.jsp
+++ src/main/webapp/WEB-INF/views/admin/infra/codeGroup/codeGroupList.jsp
@@ -4,4 +4,42 @@
 <%@ taglib prefix="fn" uri="http://java.sun.com/jsp/jstl/functions" %>
 <%@ taglib prefix="rb" uri="http://www.springframework.org/tags" %>
 
-ttttttttttttt
+<!DOCTYPE html>
+
+<table class="table primary-table-bordered display"
+									style="min-width: 845px">
+									<thead class="mb-2 thead-primary">
+										<tr>
+											<th>#</th>
+											<th>CODEGROUPNAME</th>
+										</tr>
+									</thead>
+									<tbody class="mb-3">
+										<c:choose>
+											<c:when test="${fn:length(list) eq 0}">
+												<tr>
+													<td class="text-center" colspan="9">데이터가 없습니다!</td>
+												</tr>
+											</c:when>
+											<c:otherwise>
+												<%-- ${list} 자바에서 넘겨준 객체 이름 --%>
+												<!-- var="list" jstl 블럭에서 사용할 변수 이름 -->
+												<c:forEach items="${list}" var="list" varStatus="status">
+													<tr>
+														<td><a
+															href="/cgform?cgSeq=<c:out value="${list.cgSeq}"></c:out>">
+																<c:out value="${list.cgSeq}"></c:out>
+														</a></td>
+														<td><a
+															href="/cgform?cgSeq=<c:out value="${list.cgSeq}"></c:out>">
+																<c:out value="${list.cgName}"></c:out>
+														</a></td>
+													</tr>
+												</c:forEach>
+											</c:otherwise>
+										</c:choose>
+									</tbody>
+									<tfoot>
+										
+									</tfoot>
+								</table>
