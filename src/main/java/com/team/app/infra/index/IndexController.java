6c54100ba6b8c03739979ae5dfce6f31d9452894
singasong127 <songjy0127@naver.com>
2023-08-08 17:36:42 +0900

    da

--- src/main/java/com/team/app/infra/index/IndexController.java
+++ src/main/java/com/team/app/infra/index/IndexController.java
@@ -14,7 +14,6 @@ import com.team.app.infra.party.PartyVo;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class IndexController {
